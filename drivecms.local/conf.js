2a202ea22b138572312d1f851dea914c925566df
Marcos Caputo <caputo.marcos@gmail.com>
2020-01-11 12:22:52 -0300

    first commit

--- drivecms.local/conf.js
+++ drivecms.local/conf.js
@@ -1,6 +1,6 @@
 export default {
     "author": "React Drive CMS",
-    "dashboardId": "1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4",
+    "dashboardId": "11DocbzuH3yM0TXEi_mX7S5DSSpMUG_01bTWSoqilU0U",
     "sendContactMessageUrlId": "AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d",
     "shortname": "easydrivecms",
     "root": "blog",
